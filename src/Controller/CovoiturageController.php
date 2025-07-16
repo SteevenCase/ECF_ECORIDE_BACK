cad802055789de79a873afa12c1b25eea799138d
SteevenG <roxas76500@live.fr>
2025-07-16 13:07:44 +0200

    fonction show pour voir les covoiturages

--- src/Controller/CovoiturageController.php
+++ src/Controller/CovoiturageController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\CovoiturageRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -14,5 +16,25 @@ final class CovoiturageController extends AbstractController
         return $this->render('covoiturage/index.html.twig', [
             'controller_name' => 'CovoiturageController',
         ]);
+    }
+
+    #[Route('/recherche-covoiturage', name: 'recherche_covoiturage', methods: ['POST'])]
+    public function Show(Request $request, CovoiturageRepository $repo): Response
+    {
+        $lieuDepart = $request->request->get(key: 'lieuDepart');
+        $lieuArrivee = $request->request->get(key: 'lieuArrivee');
+        $date = $request->request->get(key: 'dateDepart');
+
+        $resultats = $repo->findBy(criteria: [
+            'lieuDepart' => $lieuDepart,
+            'lieuArrivee' => $lieuArrivee,
+            'dateDepart' => $date
+        ]);
+
+        return $this->render(view: 'covoiturage/index.html.twig',
+        parameters: [
+            'resultats' => $resultats,
+        ]);
+
     }
 }
